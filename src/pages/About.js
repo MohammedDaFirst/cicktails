10c14de53f12ca71e313eefc73075d980c09568e
lucian <luciferiizero@gmail.com>
2022-07-04 14:43:33 +0300

    first

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aut officiis maiores non quasi suscipit sapiente obcaecati esse, sequi odio magnam assumenda neque soluta laboriosam aliquam tempore at nisi natus repudiandae veniam. Aut repudiandae quaerat nam ipsum unde iure quis soluta explicabo ea hic, eos rerum ipsam. Officia, in mollitia quo debitis rem natus quis.
+      </p>
+    </section>
+  )
+}
+
+export default About
---
+++ src/pages/Error.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <div className="error-page section">
+      <div className="error-container">
+        <h1>Oops! It's a dead end</h1>      <Link to='/' className='btn btn-primary'>
+          Go Home
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Error
